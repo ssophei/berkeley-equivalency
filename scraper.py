5d288093b64190e5fe0bbdf295d75751dd284b5a
ssophei <sophie.nguyen@berkeley.edu>
2025-05-28 14:18:46 -0700

    also shows sending courses

--- scraper.py
+++ scraper.py
@@ -4,9 +4,6 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
-def filter(tag):
-    return ()
-
 def scrape():
     url = ('https://assist.org/transfer/results?year=75&institution=79&agreement=58&agreementType=from&viewAgreementsOptions=true&view=agreement&viewBy=major&viewSendingAgreements=false&viewByKey=75%2F58%2Fto%2F79%2FMajor%2Ffc50cced-05c2-43c7-7dd5-08dcb87d5deb')
     options = Options()
@@ -17,10 +14,12 @@ def scrape():
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     courses = soup.find_all('div', class_='courseLine')
     for course in courses:
+        course_number = course.find_next(class_='prefixCourseNumber')
+        course_title = course.find_next(class_='courseTitle')
         if course.find_parent(class_='rowReceiving'):
-            receiving_course_number = course.find_next(class_='prefixCourseNumber')
-            recieving_course_title = course.find_next(class_='courseTitle')
-            print(f'{receiving_course_number.text.strip()}: {recieving_course_title.text.strip()}')
+            print(f'Receiving - {course_number.text.strip()}: {course_title.text.strip()}')
+        else: 
+            print(f'Sending - {course_number.text.strip()}: {course_title.text.strip()}')
 
 if __name__ == '__main__':
     scrape()
