7126fbf3f1d3ef89b62cdfc39c3c54ef1a5c92cf
ssophei <sophie.nguyen@berkeley.edu>
2025-05-28 14:07:04 -0700

    collects all berkeley courses in agreement

--- scraper.py
+++ scraper.py
@@ -1,17 +1,25 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import requests
 from bs4 import BeautifulSoup
 import time
 
+def filter(tag):
+    return ()
+
 def scrape():
     url = ('https://assist.org/transfer/results?year=75&institution=79&agreement=58&agreementType=from&viewAgreementsOptions=true&view=agreement&viewBy=major&viewSendingAgreements=false&viewByKey=75%2F58%2Fto%2F79%2FMajor%2Ffc50cced-05c2-43c7-7dd5-08dcb87d5deb')
-    driver = webdriver.Chrome()
+    options = Options()
+    options.add_argument('--headless')
+    driver = webdriver.Chrome(options=options)
     driver.get(url)
     time.sleep(5)
     soup = BeautifulSoup(driver.page_source, 'html.parser')
-    courses = soup.find_all('div', class_='courseTitle')
-    titles = [course_div.text.strip() for course_div in courses]
-    print(titles)
+    courses = soup.find_all('div', class_='courseLine')
+    for course in courses:
+        if course.find_parent(class_='rowReceiving'):
+            receiving_course = course.find_next(class_='prefixCourseNumber')
+            print(receiving_course.text.strip())
 
 if __name__ == '__main__':
     scrape()
