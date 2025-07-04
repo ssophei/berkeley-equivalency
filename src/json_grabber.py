eb0549a97672caedfe6c2b3dc68160187a4d8065
Devah Schaefers <73009338+devahschaefers@users.noreply.github.com>
2025-07-04 02:15:43 -0700

    intercept refactor

--- src/json_grabber.py
+++ src/json_grabber.py
@@ -1,3 +1,4 @@
+import os
 from playwright.async_api import async_playwright
 import json
 import asyncio
@@ -24,26 +25,32 @@ def unwrap_nested_json(data):
         return data
 
 async def intercept(url):
+    match_substring = 'Agreements?Key='
     async with async_playwright() as p:
         browser = await p.chromium.launch(headless=True)
         page = await browser.new_page()
 
-        json_found = asyncio.Event()
+        # Wait for the specific network response while navigating to the page.
+        async with page.expect_response(lambda r: match_substring in r.url, timeout=10000) as response_info:
+            await page.goto(url, wait_until='networkidle')
 
-        async def handle_response(response):
-            if 'Agreements?Key=' in response.url:
-                json_data = await response.json()
-                parsed_data = unwrap_nested_json(json_data)
-                with open('ivc-berkeley-ds.json', 'w', encoding='utf-8') as f:
-                    json.dump(parsed_data, f, ensure_ascii=False, indent=4)
-                json_found.set()
+        response = await response_info.value
+        json_data = await response.json()
+        parsed_data = unwrap_nested_json(json_data)
 
-        page.on('response', handle_response)
-        await page.goto(url, wait_until='networkidle')
-
-        await asyncio.wait_for(json_found.wait(), timeout=10)
         await browser.close()
+        print(json.dumps(parsed_data, indent=4))
+        
+        # print for testing
+        # filename = 'data/assist_data.json'
+        # os.makedirs(os.path.dirname(filename), exist_ok=True)
+        # with open(filename, 'w') as f:
+        #     json.dump(parsed_data, f, indent=4)
+        # print(f"Data saved to {filename}")
+
+        return parsed_data
 
 if __name__ == '__main__':
     url: str = 'https://assist.org/transfer/results?year=75&institution=124&agreement=79&agreementType=to&viewAgreementsOptions=true&view=agreement&viewBy=major&viewSendingAgreements=false&viewByKey=75%2F124%2Fto%2F79%2FMajor%2F648bdbf3-f87d-4a4e-7e0b-08dcb87d5deb'
-    asyncio.run(intercept(url))
+    result = asyncio.run(intercept(url))
+    print(result)
